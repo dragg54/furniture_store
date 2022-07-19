9e46704fbbd3babb4d42fcc5d0a9a80b7f9dda48
dragg54 <ajibolasadiq@yahoo.com>
2022-07-19 16:57:50 +0100

    added Item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Layout from './components/Layout';
-import Home from './pages/Home/Home';
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Item from "./pages/Home/Item";
 
 function App() {
   return (
     <Layout>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Item />} />
+        </Routes>
+      </Router>
     </Layout>
-    
   );
 }
 
