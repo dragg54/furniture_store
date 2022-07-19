9e2e1699c0eb1489f617acf91ce8bc7b8e2d7bb0
dragg54 <ajibolasadiq@yahoo.com>
2022-07-19 16:58:12 +0100

    added logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
   ContactContainer,
   FooterContainer,
+  FooterContent,
   FooterContentContainer,
   HelpContainer,
   IconsContainer,
@@ -10,33 +11,39 @@ import {
 
 import { MdOutlineFacebook } from "react-icons/md";
 import {  FaWhatsappSquare, FaInstagram, FaTwitter  } from "react-icons/fa";
+import { CompanyName, Img, Logo } from "../header/StyledHeader";
 
 const Footer = () => {
   return (
     <FooterContainer>
+        <FooterContent>
+        <Logo style={{alignSelf:"flex-start", marginLeft:"-40px", marginTop:"-20px", marginBottom:"0", height:"20%"}}>
+            <Img src="../../../images/logo.png"/>
+            <CompanyName>PLANKERS</CompanyName>
+        </Logo>
       <FooterContentContainer>
         <HelpContainer>
-        <h4>Help</h4>
-          <li>Contact us</li>
-          <li>Track Your Order</li>
-          <li>Returns & Exchanges</li>
-          <li>Shipping</li>
-          <li>Customer Service</li>
+        <h4 style={{opacity:"0.8"}}>Help</h4>
+          <li style={{opacity:"0.7"}}>Contact us</li>
+          <li style={{opacity:"0.7"}}>Track Your Order</li>
+          <li style={{opacity:"0.7"}}>Returns & Exchanges</li>
+          <li style={{opacity:"0.7"}}>Shipping</li>
+          <li style={{opacity:"0.7"}}>Customer Service</li>
         </HelpContainer>
         <InformationContainer>
-        <h4>Information</h4>
-          <li>About</li>
-          <li>Inspiration</li>
-          <li>Maintenance and Care</li>
-          <li>Materials</li>
-          <li>Configurator</li>
+        <h4 style={{opacity:"0.8"}}>Information</h4>
+          <li style={{opacity:"0.7"}}>About</li>
+          <li style={{opacity:"0.7"}}>Inspiration</li>
+          <li style={{opacity:"0.7"}}>Maintenance and Care</li>
+          <li style={{opacity:"0.7"}}>Materials</li>
+          <li style={{opacity:"0.7"}}>Configurator</li>
         </InformationContainer>
         <ContactContainer>
-        <h4>Contact</h4>
-          <p>1238 Fifth St. Berkeley, CA 94710.</p>
-          <p>United States</p>
+        <h4 style={{opacity:"0.8"}}>Contact</h4>
+          <p style={{opacity: "0.7"}}>1238 Fifth St. Berkeley, CA 94710.</p>
+          <p style={{opacity: "0.7"}}>United States</p>
           <p style={{color:"blue"}}>+1(603) 555 - 0123</p>
-          <p>info@plankers.com</p>
+          <p style={{opacity: "0.7"}}>info@plankers.com</p>
           <IconsContainer>
             <MdOutlineFacebook />
             <FaWhatsappSquare />
@@ -45,6 +52,7 @@ const Footer = () => {
           </IconsContainer>
         </ContactContainer>
       </FooterContentContainer>
+        </FooterContent>
     </FooterContainer>
   );
 };
