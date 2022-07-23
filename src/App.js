2ad792db124a9e6b6ba8c94cfbf3d5f43bc2cb55
dragg54 <ajibolasadiq@yahoo.com>
2022-07-23 19:40:18 +0100

    added layout to route

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Item from "./pages/Item";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <Layout>
-      <Router>
+    <Router>
+        <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/:id" element={<Item />} />
         </Routes>
-      </Router>
     </Layout>
+      </Router>
   );
 }
 
