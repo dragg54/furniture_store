e76e7ce1b83583d3feeb2c1fd222339cdd2481d0
dragg54 <ajibolasadiq@yahoo.com>
2022-07-23 20:48:26 +0100

    added layout to router

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../context/GlobalContext";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 
 const Layout = ({children}) => {
+  const context = useContext(CartContext)
+  const item = context.state
   return (
     <>
-      <Header />
-      {children}
+      <Header item={item}/>
+       {children}
       <Footer/>
     </>
   );
