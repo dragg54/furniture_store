d0350be4c8d8827c9f4122d0a592953ff49d013a
dragg54 <ajibolasadiq@yahoo.com>
2022-07-23 20:49:04 +0100

    cart context

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import { CompanyName, Img, Logo, NavContainer, NavList, SearchAndCartContainer } from './StyledHeader'
-import { FiSearch, FiShoppingCart, FiHeart } from "react-icons/fi"
+import React from "react";
+import {
+  CompanyName,
+  Img,
+  Logo,
+  NavContainer,
+  NavList,
+  SearchAndCartContainer,
+} from "./StyledHeader";
+import { FiSearch, FiShoppingCart, FiHeart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <NavContainer>
+      <Link to="/">
         <Logo>
-            <Img src="../../../images/logo.png"/>
-            <CompanyName>PLANKERS</CompanyName>
+          <Img src="../../../images/logo.png" />
+          <CompanyName>PLANKERS</CompanyName>
         </Logo>
-        <NavList>
-            <li>ABOUT US</li>
-            <li>CATALOG</li>
-            <li>CONTACT</li>
-        </NavList>
-        <SearchAndCartContainer>
-        <FiSearch/>
-        <FiHeart/>
-        <FiShoppingCart/>
-        </SearchAndCartContainer>
+      </Link>
+      <NavList>
+        <li>ABOUT US</li>
+        <li>CATALOG</li>
+        <li>CONTACT</li>
+      </NavList>
+      <SearchAndCartContainer>
+        <FiSearch />
+        <FiHeart />
+        <Link to="/cart" style={{color: "inherit"}}>
+          <li>
+            <FiShoppingCart />
+          </li>
+        </Link>
+      </SearchAndCartContainer>
     </NavContainer>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
