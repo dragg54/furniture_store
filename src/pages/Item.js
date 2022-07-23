bb1db23bf3e78ef5352467d70e3f32f5db4b1307
dragg54 <ajibolasadiq@yahoo.com>
2022-07-23 20:51:32 +0100

    --

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import useFetchAPI, { BASE_URL } from "../API/fetchAPI";
 import ItemSection from "../components/sections/item/ItemSection";
 import { useSearchParams } from "react-router-dom";
+import  GlobalProvider, { CartContext } from "../context/GlobalContext";
+
+
 const Item = () => {
+  const dispatch = useContext(CartContext).dispatch
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
   const [item] = useFetchAPI(`${BASE_URL}?id=${id}`);
   return (
     <>
-      <ItemSection item={item}/>
+      <GlobalProvider>
+        <ItemSection context={{item,dispatch}}/>
+      </GlobalProvider>
     </>
+     
   );
 };
 
