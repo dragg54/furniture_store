2ea0cee298e9864023c11f2a6cb8f14e20199217
dragg54 <ajibolasadiq@yahoo.com>
2022-07-26 23:03:45 +0100

    --

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Item from "./pages/Item";
 import Cart from "./pages/Cart";
+import useFetchAPI, { BASE_URL } from "./API/fetchAPI";
+import Loading from "./loading/Loading";
 
 function App() {
-  return (
-    <Router>
-        <Layout>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path=":id" element={<Item />} />
-        </Routes>
-    </Layout>
-      </Router>
-  );
+
+  const [data] = useFetchAPI(BASE_URL);
+  if(data.lenght !== 0){
+    return (
+      <Router>
+          <Layout>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path=":id" element={<Item />} />
+          </Routes>
+      </Layout>
+        </Router>
+    );
+  }
+
+  else{
+    return(
+      <Loading />
+    )
+  }
+
+
 }
 
 export default App;
