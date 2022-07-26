4eb4b8ec7968101e94746a6f2467a65a057605cc
dragg54 <ajibolasadiq@yahoo.com>
2022-07-26 23:08:43 +0100

    added delete_from-cart case

--- src/context/CartReducer.js
+++ src/context/CartReducer.js
@@ -1,9 +1,16 @@
 export const ADD_TO_CART = "add_to_cart";
+export const DELETE_FROM_CART = "delete_from_cart"
 
 export const reducer = (state , action) => {
   switch (action.type) {
     case ADD_TO_CART:
        return [action.payload.items, ...state]
+
+    case DELETE_FROM_CART:
+      let newState = state.filter(items=>{
+        return items !== action.payload.items
+      })
+      return [...newState]
     default:
       return state;
   }
