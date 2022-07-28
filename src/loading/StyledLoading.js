f8bc2c998bf4066f1bc309c929f4aa4a0a76e99b
dragg54 <ajibolasadiq@yahoo.com>
2022-07-28 21:33:56 +0100

    increased container height

--- src/loading/StyledLoading.js
+++ src/loading/StyledLoading.js
@@ -3,8 +3,8 @@ import { keyframes } from "styled-components"
 
 export const LoadingContainer = styled.div`
     width: 100%;
-    height: 100vh;
-    background-color: brown;
+    height: 600px;
+    background-color: #e5e5e5;
     display: flex;
     justify-content: center;
     align-items: center;
