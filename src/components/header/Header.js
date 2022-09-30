2ea3aa11a3508cfc5f7c6a1b5610c3a3c620f619
dragg54 <ajibolasadiq@yahoo.com>
2022-09-30 03:05:54 +0100

    changed logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,22 +4,35 @@ import {
   CompanyName,
   Img,
   Logo,
+  Menu,
   NavContainer,
   NavList,
   SearchAndCartContainer,
 } from "./StyledHeader";
-import { FiSearch, FiShoppingCart, FiHeart } from "react-icons/fi";
+import { FiSearch, FiShoppingCart, FiMenu } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 const Header = ({item}) => {
   return (
     <NavContainer>
-      <Link to="/">
-        <Logo>
-          <Img src="../../../images/logo.png" />
-          <CompanyName>PLANKERS</CompanyName>
-        </Logo>
-      </Link>
+      <div
+        style={{
+          marginLeft: "35px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Menu>
+          <FiMenu />
+        </Menu>
+        <Link to="/">
+          <Logo>
+            <Img src="../../../images/logo.png" />
+            <CompanyName>PLANKERS</CompanyName>
+          </Logo>
+        </Link>
+      </div>
       <NavList>
         <li>ABOUT US</li>
         <li>CATALOG</li>
@@ -27,13 +40,10 @@ const Header = ({item}) => {
       </NavList>
       <SearchAndCartContainer>
         <FiSearch />
-        <FiHeart />
-        <Link to="/cart" style={{color: "inherit"}}>
-          <li style={{position:"relative"}}>
+        <Link to="/cart" style={{ color: "inherit" }}>
+          <li style={{ position: "relative" }}>
             <FiShoppingCart />
-            <CartItemQuantity>
-              {item.length}
-            </CartItemQuantity>
+            <CartItemQuantity>{item.length}</CartItemQuantity>
           </li>
         </Link>
       </SearchAndCartContainer>
